44cd76a989388c4f5ae2d115cb544ba0d2ab846e
Hector Cohen <hectorcohenmaldonado@gmail.com>
2024-09-27 17:30:17 -0500

    feat: add new feedback button
    Feedback button

--- pages/_document.js
+++ pages/_document.js
@@ -8,11 +8,13 @@ export default class Document extends NextDocument {
             <Html lang="en">
                 <Head>
                     <script defer data-domain="devtor-homepage.vercel.app" src="https://plausible.io/js/plausible.js"></script>
+                    <script src="https://feedback-widtget.vercel.app//widget.umd.js"></script>
                 </Head>
                 <body>
                     <ColorModeScript  initialColorMode={theme.config.initialColorMode} />
                     <Main />
                     <NextScript />
+                    <my-widget project-id="4"></my-widget>
                 </body>
             </Html>
         )
